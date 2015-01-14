efae9e250d0f963fc27362f41eb255d137d771fb
matrix01 <milon06@gmail.com>
2015-01-15 00:54:29 +0600

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.idea/workspace.xml
    	index.php

--- index.php
+++ index.php
@@ -7,7 +7,13 @@
  */
 include_once "Databases/Database.php";
 
+<<<<<<< HEAD
 $_db = new Database();
 echo 'From milan pc';
 echo "This is it";
 echo "this is not it";
+=======
+echo "Change";
+echo "This is it";
+echo "Dirty Mind Says.....";
+>>>>>>> origin/master
